bcf1a2e47c8f7446b69d38ba8a23a0d5560eb37e
uni267 <m-uenoyama@cyber-l.co.jp>
2017-06-06 17:34:45 +0900

    no recalc branche

--- primSum2.rb
+++ primSum2.rb
@@ -1,25 +1,36 @@
 require 'prime'
-@result = 0
+result = 0
 n=gets.to_i
 prims=Prime.each(n).to_a
 prims.reverse!
+@resuthash = {}
 #検索用メソッド
-def search(patr,now)  
-  patr.each{|i|    
-  #枝きり    
-  if now%2 != 0 && i==2 then
-    return
-  end
-  t = now-i
-  if t == 0 || t==2 || (now%2 == 0 && i==2) then
-    @result +=1
-  elsif t>=2 then
-    #さかのぼり防止,枝きり（重複パターン排除）もっと帰納にできるきがする
-    natr=patr.dup
-    natr.keep_if{|item| t >= item && i >= item }
-    search(natr,t)
-  end
-}
+def search(patr,now)
+  sum = 0
+  patr.each{|i|
+    if now%2 != 0 && i==2 then
+    elsif
+      t = now-i
+      if t == 0 || t==2 || (now%2 == 0 && i==2) || t==3 then
+        sum += 1
+      elsif t>=2 then
+        #さかのぼり防止（重複パターン排除）
+        natr=patr.dup
+        natr.keep_if{|item| t >= item && i >= item }
+        key = "#{t}x#{natr}"
+        if @resuthash.has_key?(key) then
+          res = @resuthash[key]
+          sum += res
+        else
+          res = search(natr,t)
+          @resuthash[key] = res
+          sum += res
+        end
+      end
+    end
+   }
+ return sum
 end
-search(prims,n)
-p @result
+result = search(prims,n)
+p result
+      
